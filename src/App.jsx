7173e32bd8705425244ce373522c9a6f046b35a9
Giorgi Gochitidze <giorgigochitidze555@gmail.com>
2023-05-19 18:36:48 +0400

    modified:   src/App.css 	modified:   src/App.jsx 	new file:   src/Home.jsx 	new file:   src/assets/html.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import Vior from './assets/vior.png'
 import './App.css'
-import { animate, motion, transform } from 'framer-motion'
+import Home from './Home'
 
 
 function App() {
 
   return (
-    <div className='container'>
-      <header>
-        <nav>
-          <img alt="a vior logo" src={Vior}/>
-          <motion.a>მთავარი</motion.a> <a>კურსები</a> <a>ჩვენს შესახებ</a> <a>კონტაქტი</a>
-        </nav>
-      </header>
-
-      <div className='courses-container'>
-        <motion.div className="drag-area"/>
-        <motion.div whileTap={{scale: 0.9, borderRadius: '50%'}} className='drag-div' drag dragConstraints={{
-          top: -50,
-          left: -50,
-          right: 50,
-          bottom: 50,
-        }}/>
-      </div>
-    </div>
+    <Home />
   )
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,56 @@
+import Vior from './assets/vior.png'
+import { motion } from 'framer-motion'
+import './App.css'
+
+function Home() {
+
+    const Html = () => (
+        <motion.h3 transition={{ duration: 0.5}} className='little-games' whileHover={{rotate: 360}}>HTML</motion.h3>
+    )
+
+    const Css = () => (
+        <motion.h3 transition={{ duration: 0.5}} className='little-games' whileHover={{rotate: 360}}>CSS</motion.h3>
+    )
+
+    const Js = () => (
+        <motion.h3 transition={{ duration: 0.5}} className='little-games' whileHover={{rotate: 360}}>Javascript</motion.h3>
+    )
+
+    const React = () => (
+        <motion.h3 transition={{ duration: 0.5}} className='little-games' whileHover={{rotate: 360}}>React JS</motion.h3>
+    )
+
+    return(
+    <div className='container'>
+      <header>
+        <nav>
+          <img alt="a vior logo" src={Vior}/>
+          <a>მთავარი</a> <a>კურსები</a> <a>ჩვენს შესახებ</a> <a>კონტაქტი</a>
+        </nav>
+      </header>
+
+      <div className='body-container'>
+        <div className='aboutus-main-container'>
+            <div>
+                <h3 style={{paddingInline: '15px'}}>ვინ ვართ ჩვენ?</h3>
+                <img style={{borderRadius: '20px'}} alt="a vior logo" src={Vior}/>
+            </div>
+
+            <div className='about-us-content'>
+                <div style={{overflow: 'hidden', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '100%', height: '70px'}}>
+                    <Html /> <React />
+                </div>
+                <p>ჩვენ ვართ გუნდი რომელიც ცდილობს მიაწოდოს ახალ
+                ადამიანებს ამ სფეროში საჭირო მასალები და დააკვალიანოს
+                ისინი სწორ გზაზე</p>
+                <div style={{overflow: 'hidden', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', width: '100%'}}>
+                <Css /> <Js />
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default Home
